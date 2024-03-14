e3aff3623851d6cf149cf871b95f888bcd229b02
raju365 <82221153+raju365@users.noreply.github.com>
2024-03-14 19:00:54 +0530

    Update Footer.jsx

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -4,7 +4,7 @@ import "../css/footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      Copyright &#169; 2023 Raju Barman. All right received.
+      Copyright &#169; 2023 Raju Barman. All Right Reserved.
     </footer>
   );
 }
